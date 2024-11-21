b5dae968444fb5c9568753312a9e02494becf6e1
Quinn D. <danielss1@etsu.edu>
2024-11-21 17:58:14 -0500

    rabbitholes......
    slash commands had me fucked up. recommendation: roll back to last commit and compare to this one

---
+++ cogs/Ping.py
@@ -0,0 +1,26 @@
+# Ping.py
+# Quinn
+# Uses tutorial: Making A Discord Bot In Python (Part 4: Cogs)
+# https://www.youtube.com/watch?v=hxsGrMijgUA&list=PLwqYQaS6jxfk_NCetUOyNRDGAf9_kU90n&index=4
+
+import discord
+from discord.ext import commands
+
+
+class Ping(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.Cog.listener()    # events use this decorator
+    async def on_ready(self):
+        print("Ping.py is ready!")
+    
+
+
+
+    
+
+
+
+async def setup(client):
+    await client.add_cog(Ping(client))      #add_cog(NAME_OF_CLASS(client))   #case sensitive
--- main.py
+++ main.py
@@ -10,25 +10,41 @@
 
 from dotenv import load_dotenv
 import discord 
+from discord import app_commands
 from discord.ext import commands, tasks
 
 from itertools import cycle
 
 import asyncio
-
+import random
 import os           #Q# os library is only used to get the TOKEN from the .env file
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 
+MY_GUILD = discord.Object(id=1182049728058380409)
+
+
+#slash commands instead of old! commands
+
+
+
 ##client = discord.Client()           #Q# creates instance of connection to Discord  ## received error: Client.__init__() missing 1 required keyword-only argument: 'intents'
 
 
-intents = discord.Intents.default()
+intents = discord.Intents.default() #all() #.default()
 # set particular Intents                        # https://discordpy.readthedocs.io/en/latest/api.html?highlight=client#intents
 intents.message_content = True
-client = discord.Client(intents=intents)        # https://stackoverflow.com/a/74331540
 
+
+bot = discord.Client(intents=intents)        # https://stackoverflow.com/a/74331540
+
+client = commands.Bot(command_prefix='=', intents=intents)     # https://discordpy.readthedocs.io/en/latest/ext/commands/commands.html#ext-commands-commands
+#tree = app_commands.CommandTree(client)
+#
+#
+# HERE https://stackoverflow.com/questions/71165431/how-do-i-make-a-working-slash-command-in-discord-py
+#
 
 #Q# More about @client.events  :     https://discordpy.readthedocs.io/en/latest/api.html#discord.Client.event
 
@@ -52,9 +68,12 @@ async def change_status():
 
 
 @client.event
-async def on_ready():       #Q# - on_ready(), on_message() is an example of an event callback, aka when something happens
+async def on_ready(self):       #Q# - on_ready(), on_message() is an example of an event callback, aka when something happens
     print('We have logged in as {0.user}'.format(client))
     change_status.start()                #Q# video: Making a Discord Bot in Python (Part 3: Activity Status)
+    await self.tree.sync(guild=MY_GUILD)#guild=discord.Object(id=Your guild id))
+    print("Ready!")
+    #await client.tree.sync() #client.tree.sync()
 
 
 @client.event
@@ -64,6 +83,55 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+
+
+@client.event
+async def on_message(message):
+    await bot.process_commands(message)
+    print(message.content)
+
+
+
+
+
+
+
+# Add the guild ids in which the slash command will appear.
+# If it should be in all, remove the argument, but note that
+# it will take some time (up to an hour) to register the
+# command if it's for all guilds.
+@tree.command(
+    name="BraxHello",
+    description="My first application Command",
+    guild=MY_GUILD #discord.Object(MY_GUILD)
+)
+async def first_command(interaction: discord.Interaction):
+    await interaction.response.send_message("Hello! I am BraxCord")
+
+
+
+
+
+# https://dev.to/mannu/4slash-commands-in-discordpy-ofl
+##```python
+#@client.tree.command(name="mannu",description="Mannu is a good boy")
+#async def slash_command(interaction:discord.Interaction):
+#    await interaction.response.send_message("Hello World!")
+#
+#
+#@client.tree.command(name="hello", description="Says hello!")
+#async def hello(interaction: discord.Interaction):
+#    await interaction.response.send_message("Hello there!")
+#
+
+#@commands.command(name='8ball')
+#async def magic_eightball(ctx, *, question):
+#    with open("discord-project-thing/responses.txt", "r") as f:        # "r" = read mode   
+#        random_responses = f.readlines()                    # file is being treated as a list
+#        response = random.choice(random_responses)
+#    
+#    await ctx.send(response)
 
 
 async def load():
