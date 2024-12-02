82b7081e7ba99bc52526dd76993c43938ff7961c
Elijah 🌱 <174156920+amadorel@users.noreply.github.com>
2024-12-01 21:22:45 -0500

    Update data_models.py
    forced update of data models

--- data_models.py
+++ data_models.py
@@ -7,8 +7,8 @@ class Base(DeclarativeBase):
 class User(Base): 
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True), 
-    display_name = Column(String), 
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    display_name = Column(String)
     user_handle = Column(String)
 
 class Guild(Base): 
@@ -20,22 +20,22 @@ class Guild(Base):
 class UserGuild(Base): 
     __tablename__ = "userguilds"
 
-    user_id = Column(Integer),
-    guild_id = Column(Integer)
+    user_id = Column(Integer, primary_key=True)
+    guild_id = Column(Integer, primary_key=True)
 
 class Responses(Base): 
     __tablename__ = "responses"
 
-    response_id = Column(Integer, autoincrement=True),
-    message_to_detect = Column(String, primary_key=True),
+    response_id = Column(Integer, autoincrement=True)
+    message_to_detect = Column(String, primary_key=True)
     output = Column(String)
 
 class Garden(Base): 
     __tablename__ = "gardens"
 
-    user_id = Column(String) 
+    user_id = Column(String, primary_key=True) 
     
 class Wallet(Base): 
     __tablename__ = "wallets"
 
-    user_id = Column(String)
+    user_id = Column(String, primary_key=True)
