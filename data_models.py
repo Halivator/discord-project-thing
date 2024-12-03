88f460afc7bc1b68122ad03726d9fdcc702ed268
Elijah 🌱 <amadorb@etsu.edu>
2024-12-03 15:20:49 -0500

    Updating and scaling back data models for bot

--- data_models.py
+++ data_models.py
@@ -4,19 +4,6 @@ from sqlalchemy.orm import DeclarativeBase
 class Base(DeclarativeBase): 
     pass
    
-class User(Base): 
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    display_name = Column(String)
-    user_handle = Column(String)
-
-class Guild(Base): 
-    __tablename__ = 'guilds'
-
-    guild_id = Column(Integer, primary_key=True, autoincrement=True)
-    guild_name = Column(String)
-
 class UserGuild(Base): 
     __tablename__ = 'userguilds'
 
@@ -29,13 +16,10 @@ class Responses(Base):
     response_id = Column(Integer, autoincrement=True)
     message_to_detect = Column(String, primary_key=True)
     output = Column(String)
-
-class Garden(Base): 
-    __tablename__ = 'gardens'
-
-    user_id = Column(String, primary_key=True) 
     
 class Wallet(Base): 
     __tablename__ = 'wallets'
 
-    user_id = Column(String, primary_key=True)
+    user_id = Column(String, primary_key=True), 
+    balance = Column(Integer), 
+    number_of_tomatoes = Column(Integer)
